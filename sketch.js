2b2d15a015e369a26164039408e9fde67cb3475c
johanhansson <hanjoh-2@student.ltu.se>
2017-02-06 11:34:29 +0100

    Added click to toggle tiles.

--- sketch.js
+++ sketch.js
@@ -6,59 +6,101 @@ function preload() {
 }
 
 function setup() {
-  pattern = new Pattern(10, 30,30,"img", 7);
   canvas = createCanvas(700, 400);
+  pattern = new Pattern(10, 30,30,"img", 7);
+  pattern.rows = generateRows();
 }
 
 function draw() {
+    clear();
+    pattern.printRows();
+}
+
+function Tile(tileNumber, x){
+    this.visable = true;
+    this.num = tileNumber;
+    this.x = x;
+}
+
+function Row(tiles, y, offset) {
+    this.tiles = tiles;
+    this.y = y;
+    this.offset = offset;
+}
+
+
+function Pattern(padding, imgWidth, imgHeight, path, num_tiles){
+    this.tileWidth = imgWidth + padding;
+    this.tileHeight = imgHeight + (padding/2);
+    this.imgWidth = imgWidth;
+    this.imgHeight = imgHeight;
+    this.num_tiles = num_tiles;
+    this.rows = [];
+    this.images = [];
+
+    for(i = 0; i < this.num_tiles; i++){
+        this.images[i] = loadImage(path+"/"+ (i+1) +".png");
+    }
+
+    this.printRows = function () {
+        for(var i = 0; i < this.rows.length; i++) {
+            var currRow = this.rows[i];
+            for (var j = 0; j < currRow.tiles.length; j++) {
+                var currTile = currRow.tiles[j];
+                if (currTile.visable) {
+                    image(this.images[currTile.num], currTile.x, currRow.y, this.imgWidth, this.imgHeight);
+                }
+            }
+        }
+    }
+}
+
+function generateRows() {
+    var newRows = [];
     var j = 0;
     var offset = 0;
     var tile_counter_start = [0, 2, 5];
-    while((j+1)*pattern.getHeight() < height) {
-        i = 0;
-        tile_counter = tile_counter_start[0];
-        var y = j * pattern.getHeight();
-        if(offset == 0){
-            offset = pattern.getWidth()/2;
+    while ((j + 1) * pattern.tileHeight < height) {
+        var tile_counter = tile_counter_start[0];
+        var y = j * pattern.tileHeight;
+        if (offset == 0) {
+            offset = pattern.tileWidth / 2;
             tile_counter_start[3] = tile_counter_start[0];
-        }else{
+        } else {
             offset = 0;
             tile_counter_start[3] = 1 + tile_counter_start[0];
         }
         tile_counter_start.shift();
-        while ((i +1)* pattern.getWidth() < width) {
-            var x = i * pattern.getWidth() + offset;
-            //var y = (i+pattern.padding)*pattern.height;
-
-            pattern.showTile(tile_counter, x, y);
-            tile_counter++;
-            if (tile_counter >= pattern.num_tiles) {
-                tile_counter = 0;
-            }
-            i++;
-        }
+        var row = new Row(generateRow(tile_counter, offset), y, offset);
+        newRows.push(row);
         j++;
     }
-}
-function Pattern(padding, width, height, path, num_tiles){
-    this.padding = padding;
-    this.width = width;
-    this.height = height;
-    this.num_tiles = num_tiles;
-    this.tiles = [];
-    this.getWidth = function () {
-      return this.width+this.padding;
-    }
-    this.getHeight = function () {
-        return this.height+(this.padding/2);
-    }
-    for(i = 0; i < this.num_tiles; i++){
-    this.tiles[i] = loadImage(path+"/"+ (i+1) +".png", function(){
+    return newRows;
 
-    });
+}
+function generateRow (startTile, offset) {
+    var newTiles = [];
+    var x = offset;
+    var tileNum = startTile;
+    while((x+pattern.tileWidth) < width){
+        newTiles.push(new Tile(tileNum, x));
+        tileNum++;
+        if(tileNum >= pattern.num_tiles){
+            tileNum = 0;
+        }
+        x += pattern.tileWidth;
     }
-    this.showTile = function(num_tile, x, y){
-        var n = num_tile;
-        image(this.tiles[n], x, y, this.width, this.height);
+    return newTiles;
+}
+
+function mousePressed() {
+    // Find closeset tile
+    var y = floor(mouseY/pattern.tileHeight);
+    var x = floor((mouseX-pattern.rows[y].offset)/pattern.tileWidth);
+    // Check if click was inside circle
+    var d = dist(mouseX, mouseY, pattern.rows[y].tiles[x].x+pattern.imgWidth/2, pattern.rows[y].y+pattern.imgWidth/2);
+    if (d < (pattern.imgWidth/2) ) {
+        pattern.rows[y].tiles[x].visable = !pattern.rows[y].tiles[x].visable;
     }
+
 }
